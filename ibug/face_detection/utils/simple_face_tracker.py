9e38dc5413c22e2e3e2dbd5208f36c72d11fd8bd
hhj1897 <js1907@imperial.ac.uk>
2021-04-30 14:09:28 +0100

    SimpleFaceTracker can now handle empty list.

--- ibug/face_detection/utils/simple_face_tracker.py
+++ ibug/face_detection/utils/simple_face_tracker.py
@@ -30,6 +30,9 @@ class SimpleFaceTracker(object):
         self._minimum_face_size = face_size
 
     def __call__(self, face_boxes: np.ndarray) -> List[Optional[int]]:
+        if face_boxes.size <= 0:
+            return []
+
         # Calculate area of the faces
         face_areas = np.abs((face_boxes[:, 2] - face_boxes[:, 0]) * (face_boxes[:, 3] - face_boxes[:, 1]))
 
