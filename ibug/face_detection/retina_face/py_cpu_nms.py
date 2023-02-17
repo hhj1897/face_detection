Fix the area calculation in the NMS code. (fixes #2)

ibug/face_detection/retina_face/py_cpu_nms.py
@@ -16,7 +16,7 @@ def py_cpu_nms(dets, thresh, top_k):
     y2 = dets[:, 3]
     scores = dets[:, 4]
 
-    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    areas = (x2 - x1) * (y2 - y1)
     order = scores.argsort()[: -top_k - 1: -1]
 
     keep = []
@@ -28,8 +28,8 @@ def py_cpu_nms(dets, thresh, top_k):
         xx2 = np.minimum(x2[i], x2[order[1:]])
         yy2 = np.minimum(y2[i], y2[order[1:]])
 
-        w = np.maximum(0.0, xx2 - xx1 + 1)
-        h = np.maximum(0.0, yy2 - yy1 + 1)
+        w = np.maximum(0.0, xx2 - xx1)
+        h = np.maximum(0.0, yy2 - yy1)
         inter = w * h
         ovr = inter / (areas[i] + areas[order[1:]] - inter)
 